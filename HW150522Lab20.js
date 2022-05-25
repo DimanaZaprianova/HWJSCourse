42b2f660122c451a200a20f556acd1bd9c9bddfe
DimanaZaprianova <105113180+DimanaZaprianova@users.noreply.github.com>
2022-05-25 18:05:41 +0300

    Add files via upload

---
+++ HW150522Lab20.js
@@ -0,0 +1,35 @@
+// let manHeight;
+// let manWeight;
+// let BMI;
+// manHeight = 1.70;
+// manWeight = 77;
+// BMI = manWeight/manHeight**2;
+
+// console.log('The BMI is: ' + (BMI).toFixed(2));
+
+
+// // HW1: Comparison Operators & Conditional Statement
+// let userTown = "Париж";
+// if(userTown ==="София"){
+//     console.log("Здравей!")
+// }
+// else if(userTown === "Париж"){
+//     console.log("Salut!")
+// }
+// else {
+//     console.log("Hi!")
+// };
+
+// let sum = 0;
+// for(let i = 1; i <= 20; i++) {
+//     if (i%2 == 0 && i%3 == 0) {
+//         sum = sum + i
+//     };
+// };
+// console.log(sum);
+
+// var treeDeep = 5;
+// for (var i = 1; i <= treeDeep; i++) {
+//   console.log("*".repeat(i));
+// }
+
